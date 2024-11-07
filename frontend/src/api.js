3e6d5a3cf5e57795e1ea31f51b14ecfa0b063f0a
muniss950 <munish3971@gmail.com>
2024-11-07 19:24:06 +0530

    minor changes

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000'; // Replace with your Flask server URL
-
-export const getTeams = () => axios.get(`${API_URL}/teams`);
-export const getPlayers = () => axios.get(`${API_URL}/players`);
-export const getMatches = () => axios.get(`${API_URL}/matches`);
-export const getTournaments = () => axios.get(`${API_URL}/tournaments`);
-
