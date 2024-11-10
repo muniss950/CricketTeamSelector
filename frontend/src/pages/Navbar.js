b42fb0d424e3553f1bd19a18e47cf75cf5ecbc9b
Mayank Sharma <m4945sharma@gmail.com>
2024-11-10 23:58:14 +0530

    minor glitch fixing

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -1,13 +1,13 @@
 // Import Link from react-router-dom
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Navbar = () => (
     <nav className="App-navbar">
         <ul>
-            <li><Link to="/teams">Teams</Link></li>
-            <li><Link to="/players">Players</Link></li>
-            <li><Link to="/matches">Matches</Link></li>
-            <li><Link to="/tournaments">Tournaments</Link></li>
+            {/* <li><Link to="/teams">Teams</Link></li> */}
+            {/* <li><Link to="/players">Players</Link></li> */}
+            {/* <li><Link to="/matches">Matches</Link></li> */}
+            {/* <li><Link to="/tournaments">Tournaments</Link></li> */}
         </ul>
     </nav>
 );
