99326134eef920672cf6aca0116de8d819ff2622
muniss950 <munish3971@gmail.com>
2024-11-12 20:16:06 +0530

    player name in scorecard

--- backend/models/batting.py
+++ backend/models/batting.py
@@ -1,10 +1,8 @@
-# # models/batting.py
-
 # import mysql.connector
 # from config import db_config
-
+#
 # class Batting:
-#     def __init__(self, player_id, match_id,inning_number, runs_scored=0, balls_faced=0, fours=0, sixes=0, position=-1):
+#     def __init__(self, player_id, match_id, inning_number, runs_scored=0, balls_faced=0, fours=0, sixes=0, position=-1):
 #         self.player_id = player_id
 #         self.match_id = match_id
 #         self.runs_scored = runs_scored
@@ -12,8 +10,8 @@
 #         self.fours = fours
 #         self.sixes = sixes
 #         self.position = position
-#         self.inning_number=inning_number
-
+#         self.inning_number = inning_number
+#
 #     @staticmethod
 #     def get_batting_stats():
 #         """Fetch all batting stats from the Batting table."""
@@ -24,12 +22,25 @@
 #         cursor.close()
 #         connection.close()
 #         return batting_data
+#
+#     @staticmethod
+#     def get_batting_stats_by_player(player_id):
+#         """Fetch all batting stats for a specific player."""
+#         connection = mysql.connector.connect(**db_config)
+#         cursor = connection.cursor(dictionary=True)
+#         cursor.execute('SELECT * FROM Batting WHERE Player_ID = %s', (player_id,))
+#         batting_data = cursor.fetchall()
+#         cursor.close()
+#         connection.close()
+#         return batting_data
+#
 #     @staticmethod
 #     def get_batting_scorecard(match_id, inning_number):
+#         """Fetch scorecard for a particular match and inning."""
 #         connection = mysql.connector.connect(**db_config)
 #         cursor = connection.cursor(dictionary=True)
 #         query = """
-#             SELECT Player_ID, Runs_Scored, Balls_Faced, Fours, Sixes,Position
+#             SELECT Player_ID, Runs_Scored, Balls_Faced, Fours, Sixes, Position
 #             FROM Batting
 #             WHERE Match_ID = %s AND Inning_Number = %s
 #         """
@@ -38,6 +49,7 @@
 #         cursor.close()
 #         connection.close()
 #         return batting_data
+#
 #     @staticmethod
 #     def get_batting_stats_by_id(player_id, match_id):
 #         """Fetch a specific batting entry by player and match ID."""
@@ -48,56 +60,55 @@
 #         cursor.close()
 #         connection.close()
 #         return batting_data
-
+#
 #     @staticmethod
 #     def add_batting_stats(batting):
 #         """Add a new batting entry to the Batting table."""
 #         connection = mysql.connector.connect(**db_config)
 #         cursor = connection.cursor()
-#         cursor.execute('''
-#             INSERT INTO Batting (Player_ID, Match_ID, Runs_Scored, Balls_Faced, Fours, Sixes, Position,Inning_Number)
+#         cursor.execute(''' 
+#             INSERT INTO Batting (Player_ID, Match_ID, Runs_Scored, Balls_Faced, Fours, Sixes, Position, Inning_Number) 
 #             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-#         ''', (batting.player_id, batting.match_id, batting.runs_scored, batting.balls_faced,
-#               batting.fours, batting.sixes, batting.position,batting.inning_number))
+#         ''', (batting.player_id, batting.match_id, batting.runs_scored, batting.balls_faced, 
+#               batting.fours, batting.sixes, batting.position, batting.inning_number))
 #         connection.commit()
 #         cursor.close()
 #         connection.close()
-
+#
 #     @staticmethod
-#     def update_batting_stats(player_id, match_id, inning_number,runs_scored=None, balls_faced=None, fours=None, sixes=None, position=None):
+#     def update_batting_stats(player_id, match_id, inning_number, runs_scored=None, balls_faced=None, fours=None, sixes=None, position=None):
 #         """Update an existing batting entry in the Batting table."""
 #         connection = mysql.connector.connect(**db_config)
 #         cursor = connection.cursor()
-#         cursor.execute('''
-#             UPDATE Batting
-#             SET Runs_Scored = COALESCE(%s, Runs_Scored),
-#                 Balls_Faced = COALESCE(%s, Balls_Faced),
-#                 Fours = COALESCE(%s, Fours),
-#                 Sixes = COALESCE(%s, Sixes),
-#                 Position = COALESCE(%s, Position)
-#                 Inning_Number=COALESCE(%s, Inning_Number)
-#             WHERE Player_ID = %s AND Match_ID = %s
-#         ''', (runs_scored, balls_faced, fours, sixes, position, player_id, match_id,inning_number))
+#         cursor.execute(''' 
+#             UPDATE Batting 
+#             SET Runs_Scored = COALESCE(%s, Runs_Scored), 
+#                 Balls_Faced = COALESCE(%s, Balls_Faced), 
+#                 Fours = COALESCE(%s, Fours), 
+#                 Sixes = COALESCE(%s, Sixes), 
+#                 Position = COALESCE(%s, Position),
+#                 Inning_Number = COALESCE(%s, Inning_Number)
+#             WHERE Player_ID = %s AND Match_ID = %s AND Inning_Number = %s
+#         ''', (runs_scored, balls_faced, fours, sixes, position, inning_number, player_id, match_id))
 #         connection.commit()
 #         cursor.close()
 #         connection.close()
-
+#
 #     @staticmethod
-#     def delete_batting_stats(player_id, match_id,inning_number):
+#     def delete_batting_stats(player_id, match_id, inning_number):
 #         """Delete a specific batting entry by player and match ID."""
 #         connection = mysql.connector.connect(**db_config)
 #         cursor = connection.cursor()
-#         cursor.execute('DELETE FROM Batting WHERE Player_ID = %s AND Match_ID = %s AND Inning_Number = %s', (player_id, match_id,inning_number))
+#         cursor.execute('DELETE FROM Batting WHERE Player_ID = %s AND Match_ID = %s AND Inning_Number = %s', (player_id, match_id, inning_number))
 #         connection.commit()
 #         cursor.close()
 #         connection.close()
-
-
+#
 import mysql.connector
 from config import db_config
 
 class Batting:
-    def __init__(self, player_id, match_id, inning_number, runs_scored=0, balls_faced=0, fours=0, sixes=0, position=-1):
+    def __init__(self, player_id, match_id, inning_number, player_name=None, runs_scored=0, balls_faced=0, fours=0, sixes=0, position=-1):
         self.player_id = player_id
         self.match_id = match_id
         self.runs_scored = runs_scored
@@ -106,13 +117,18 @@ class Batting:
         self.sixes = sixes
         self.position = position
         self.inning_number = inning_number
+        self.player_name = player_name  # Added player_name
 
     @staticmethod
     def get_batting_stats():
-        """Fetch all batting stats from the Batting table."""
+        """Fetch all batting stats along with player names from the Batting table."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM Batting')
+        cursor.execute('''
+            SELECT Batting.*, Player.Player_Name
+            FROM Batting
+            JOIN Player ON Batting.Player_ID = Player.Player_ID
+        ''')
         batting_data = cursor.fetchall()
         cursor.close()
         connection.close()
@@ -120,10 +136,15 @@ class Batting:
 
     @staticmethod
     def get_batting_stats_by_player(player_id):
-        """Fetch all batting stats for a specific player."""
+        """Fetch all batting stats for a specific player along with the player's name."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM Batting WHERE Player_ID = %s', (player_id,))
+        cursor.execute('''
+            SELECT Batting.*, Player.Player_Name
+            FROM Batting
+            JOIN Player ON Batting.Player_ID = Player.Player_ID
+            WHERE Batting.Player_ID = %s
+        ''', (player_id,))
         batting_data = cursor.fetchall()
         cursor.close()
         connection.close()
@@ -131,14 +152,16 @@ class Batting:
 
     @staticmethod
     def get_batting_scorecard(match_id, inning_number):
-        """Fetch scorecard for a particular match and inning."""
+        """Fetch scorecard for a particular match and inning along with player names."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor(dictionary=True)
-        query = """
-            SELECT Player_ID, Runs_Scored, Balls_Faced, Fours, Sixes, Position
+        query = '''
+            SELECT Batting.Player_ID, Player.Player_Name, Batting.Runs_Scored, Batting.Balls_Faced, 
+                   Batting.Fours, Batting.Sixes, Batting.Position
             FROM Batting
-            WHERE Match_ID = %s AND Inning_Number = %s
-        """
+            JOIN Player ON Batting.Player_ID = Player.Player_ID
+            WHERE Batting.Match_ID = %s AND Batting.Inning_Number = %s
+        '''
         cursor.execute(query, (match_id, inning_number))
         batting_data = cursor.fetchall()
         cursor.close()
@@ -147,10 +170,15 @@ class Batting:
 
     @staticmethod
     def get_batting_stats_by_id(player_id, match_id):
-        """Fetch a specific batting entry by player and match ID."""
+        """Fetch a specific batting entry by player and match ID along with the player's name."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM Batting WHERE Player_ID = %s AND Match_ID = %s', (player_id, match_id))
+        cursor.execute('''
+            SELECT Batting.*, Player.Player_Name
+            FROM Batting
+            JOIN Player ON Batting.Player_ID = Player.Player_ID
+            WHERE Batting.Player_ID = %s AND Batting.Match_ID = %s
+        ''', (player_id, match_id))
         batting_data = cursor.fetchone()
         cursor.close()
         connection.close()
@@ -158,10 +186,10 @@ class Batting:
 
     @staticmethod
     def add_batting_stats(batting):
-        """Add a new batting entry to the Batting table."""
+        """Add a new batting entry to the Batting table along with the player name."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
-        cursor.execute(''' 
+        cursor.execute('''
             INSERT INTO Batting (Player_ID, Match_ID, Runs_Scored, Balls_Faced, Fours, Sixes, Position, Inning_Number) 
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         ''', (batting.player_id, batting.match_id, batting.runs_scored, batting.balls_faced, 
@@ -172,10 +200,10 @@ class Batting:
 
     @staticmethod
     def update_batting_stats(player_id, match_id, inning_number, runs_scored=None, balls_faced=None, fours=None, sixes=None, position=None):
-        """Update an existing batting entry in the Batting table."""
+        """Update an existing batting entry in the Batting table along with the player name."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
-        cursor.execute(''' 
+        cursor.execute('''
             UPDATE Batting 
             SET Runs_Scored = COALESCE(%s, Runs_Scored), 
                 Balls_Faced = COALESCE(%s, Balls_Faced), 
@@ -194,7 +222,10 @@ class Batting:
         """Delete a specific batting entry by player and match ID."""
         connection = mysql.connector.connect(**db_config)
         cursor = connection.cursor()
-        cursor.execute('DELETE FROM Batting WHERE Player_ID = %s AND Match_ID = %s AND Inning_Number = %s', (player_id, match_id, inning_number))
+        cursor.execute('''
+            DELETE FROM Batting 
+            WHERE Player_ID = %s AND Match_ID = %s AND Inning_Number = %s
+        ''', (player_id, match_id, inning_number))
         connection.commit()
         cursor.close()
         connection.close()
--- backend/models/bowling.py
+++ backend/models/bowling.py
@@ -1,3 +1,86 @@
+# from config import get_db_connection
+#
+# class Bowling:
+#     @staticmethod
+#     def get_bowling_stats():
+#         connection = get_db_connection()
+#         with connection.cursor(dictionary=True) as cursor:
+#             cursor.execute("SELECT * FROM Bowling")
+#             return cursor.fetchall()
+#     @staticmethod
+#     def get_bowling_scorecard(match_id, inning_number):
+#         connection = get_db_connection()
+#         cursor = connection.cursor(dictionary=True)
+#         query = """
+#             SELECT Player_ID, Overs_Bowled, Balls_Bowled, Runs_Conceded, Wickets_Taken
+#             FROM Bowling
+#             WHERE Match_ID = %s AND Inning_Number = %s
+#         """
+#         cursor.execute(query, (match_id, inning_number))
+#         bowling_data = cursor.fetchall()
+#         cursor.close()
+#         connection.close()
+#         return bowling_data
+#
+#     @staticmethod
+#     def get_bowling_stats_by_player(match_id, player_id):
+#         connection = get_db_connection()
+#         with connection.cursor(dictionary=True) as cursor:
+#             cursor.execute("SELECT * FROM Bowling WHERE Match_ID = %s AND Player_ID = %s", (match_id, player_id))
+#             return cursor.fetchone()
+#
+#     @staticmethod
+#     def add_bowling_stats(bowling_data):
+#         connection = get_db_connection()
+#         with connection.cursor() as cursor:
+#             cursor.execute(
+#                 """
+#                 INSERT INTO Bowling (Player_ID, Match_ID,Inning_Number, Overs_Bowled, Balls_Bowled, Runs_Conceded, Wickets_Taken, Maiden_Overs)
+#                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+#                 """,
+#                 (
+#                     bowling_data['Player_ID'], bowling_data['Match_ID'], bowling_data['Inning_Number'], bowling_data['Overs_Bowled'],
+#                     bowling_data['Balls_Bowled'], bowling_data['Runs_Conceded'], bowling_data['Wickets_Taken'],
+#                     bowling_data['Maiden_Overs']
+#                 )
+#             )
+#             connection.commit()
+#
+#     @staticmethod
+#     def update_bowling_stats(match_id, player_id,inning_number, bowling_data):
+#         connection = get_db_connection()
+#         with connection.cursor() as cursor:
+#             query = """
+#                 UPDATE Bowling 
+#                 SET 
+#                     Overs_Bowled = COALESCE(%s, Overs_Bowled),
+#                     Balls_Bowled = COALESCE(%s, Balls_Bowled),
+#                     Runs_Conceded = COALESCE(%s, Runs_Conceded),
+#                     Wickets_Taken = COALESCE(%s, Wickets_Taken),
+#                     Maiden_Overs = COALESCE(%s, Maiden_Overs)
+#                 WHERE Match_ID = %s AND Player_ID = %s AND Inning_Number= %s
+#             """
+#             cursor.execute(
+#                 query,
+#                 (
+#                     bowling_data.get('Overs_Bowled'), 
+#                     bowling_data.get('Balls_Bowled'), 
+#                     bowling_data.get('Runs_Conceded'),
+#                     bowling_data.get('Wickets_Taken'), 
+#                     bowling_data.get('Maiden_Overs'), 
+#                     match_id, 
+#                     player_id,
+#                     inning_number
+#                 )
+#             )
+#             connection.commit()
+#     @staticmethod
+#     def delete_bowling_stats(match_id, player_id,inning_number):
+#         connection = get_db_connection()
+#         with connection.cursor() as cursor:
+#             cursor.execute("DELETE FROM Bowling WHERE Match_ID = %s AND Player_ID = %s AND Inning_Number= %s", (match_id, player_id,inning_number))
+#             connection.commit()
+#
 from config import get_db_connection
 
 class Bowling:
@@ -5,16 +88,23 @@ class Bowling:
     def get_bowling_stats():
         connection = get_db_connection()
         with connection.cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT * FROM Bowling")
+            cursor.execute("""
+                SELECT Bowling.*, Player.Player_Name
+                FROM Bowling
+                JOIN Player ON Bowling.Player_ID = Player.Player_ID
+            """)
             return cursor.fetchall()
+
     @staticmethod
     def get_bowling_scorecard(match_id, inning_number):
         connection = get_db_connection()
         cursor = connection.cursor(dictionary=True)
         query = """
-            SELECT Player_ID, Overs_Bowled, Balls_Bowled, Runs_Conceded, Wickets_Taken
+            SELECT Bowling.Player_ID, Player.Player_Name, 
+                   Bowling.Overs_Bowled, Bowling.Balls_Bowled, Bowling.Runs_Conceded, Bowling.Wickets_Taken
             FROM Bowling
-            WHERE Match_ID = %s AND Inning_Number = %s
+            JOIN Player ON Bowling.Player_ID = Player.Player_ID
+            WHERE Bowling.Match_ID = %s AND Bowling.Inning_Number = %s
         """
         cursor.execute(query, (match_id, inning_number))
         bowling_data = cursor.fetchall()
@@ -26,7 +116,12 @@ class Bowling:
     def get_bowling_stats_by_player(match_id, player_id):
         connection = get_db_connection()
         with connection.cursor(dictionary=True) as cursor:
-            cursor.execute("SELECT * FROM Bowling WHERE Match_ID = %s AND Player_ID = %s", (match_id, player_id))
+            cursor.execute("""
+                SELECT Bowling.*, Player.Player_Name
+                FROM Bowling
+                JOIN Player ON Bowling.Player_ID = Player.Player_ID
+                WHERE Bowling.Match_ID = %s AND Bowling.Player_ID = %s
+            """, (match_id, player_id))
             return cursor.fetchone()
 
     @staticmethod
@@ -35,19 +130,19 @@ class Bowling:
         with connection.cursor() as cursor:
             cursor.execute(
                 """
-                INSERT INTO Bowling (Player_ID, Match_ID,Inning_Number, Overs_Bowled, Balls_Bowled, Runs_Conceded, Wickets_Taken, Maiden_Overs)
+                INSERT INTO Bowling (Player_ID, Match_ID, Inning_Number, Overs_Bowled, Balls_Bowled, Runs_Conceded, Wickets_Taken, Maiden_Overs)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                 """,
                 (
-                    bowling_data['Player_ID'], bowling_data['Match_ID'], bowling_data['Inning_Number'], bowling_data['Overs_Bowled'],
-                    bowling_data['Balls_Bowled'], bowling_data['Runs_Conceded'], bowling_data['Wickets_Taken'],
-                    bowling_data['Maiden_Overs']
+                    bowling_data['Player_ID'], bowling_data['Match_ID'], bowling_data['Inning_Number'],
+                    bowling_data['Overs_Bowled'], bowling_data['Balls_Bowled'], bowling_data['Runs_Conceded'],
+                    bowling_data['Wickets_Taken'], bowling_data['Maiden_Overs']
                 )
             )
             connection.commit()
 
     @staticmethod
-    def update_bowling_stats(match_id, player_id,inning_number, bowling_data):
+    def update_bowling_stats(match_id, player_id, inning_number, bowling_data):
         connection = get_db_connection()
         with connection.cursor() as cursor:
             query = """
@@ -58,25 +153,27 @@ class Bowling:
                     Runs_Conceded = COALESCE(%s, Runs_Conceded),
                     Wickets_Taken = COALESCE(%s, Wickets_Taken),
                     Maiden_Overs = COALESCE(%s, Maiden_Overs)
-                WHERE Match_ID = %s AND Player_ID = %s AND Inning_Number= %s
+                WHERE Match_ID = %s AND Player_ID = %s AND Inning_Number = %s
             """
             cursor.execute(
                 query,
                 (
-                    bowling_data.get('Overs_Bowled'), 
-                    bowling_data.get('Balls_Bowled'), 
+                    bowling_data.get('Overs_Bowled'),
+                    bowling_data.get('Balls_Bowled'),
                     bowling_data.get('Runs_Conceded'),
-                    bowling_data.get('Wickets_Taken'), 
-                    bowling_data.get('Maiden_Overs'), 
-                    match_id, 
+                    bowling_data.get('Wickets_Taken'),
+                    bowling_data.get('Maiden_Overs'),
+                    match_id,
                     player_id,
                     inning_number
                 )
             )
             connection.commit()
+
     @staticmethod
-    def delete_bowling_stats(match_id, player_id,inning_number):
+    def delete_bowling_stats(match_id, player_id, inning_number):
         connection = get_db_connection()
         with connection.cursor() as cursor:
-            cursor.execute("DELETE FROM Bowling WHERE Match_ID = %s AND Player_ID = %s AND Inning_Number= %s", (match_id, player_id,inning_number))
+            cursor.execute("DELETE FROM Bowling WHERE Match_ID = %s AND Player_ID = %s AND Inning_Number = %s", 
+                           (match_id, player_id, inning_number))
             connection.commit()
