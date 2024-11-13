38a1712ec413f1328ed8a4ad38c5471a4c98a316
muniss950 <munish3971@gmail.com>
2024-11-13 19:45:34 +0530

    crud on match

--- backend/config.py
+++ backend/config.py
@@ -4,8 +4,8 @@ import mysql.connector
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    # 'password': os.environ.get('DB_PASSWORD','12345'),
-    'password': 'kohli6639',
+    'password': os.environ.get('DB_PASSWORD','12345'),
+    # 'password': 'kohli6639',
     'database': 'cricket',
     'charset': 'utf8mb4',  # Setting charset to utf8mb4
     'collation': 'utf8mb4_unicode_ci'  # Use a compatible collation
