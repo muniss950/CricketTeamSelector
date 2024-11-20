588e57636e4e7d41515ee0930b2c727478f1db52
muniss950 <munish3971@gmail.com>
2024-11-21 00:09:40 +0530

    login button

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom';
 
 import logo from '../logo1.png'; // Import your logo
 const Navbar = () => (
-      <header className="App-header">
-        <div className="logo-container">
-          <img src={logo} alt="CricFreak Logo" className="App-logo" />
-          <h1>CRICFREAK</h1>
-        </div>
-        <nav className="App-navbar">
-          <ul>
-            <li><Link to="/teams">Teams</Link></li>
-            <li><Link to="/players">Players</Link></li>
-            <li><Link to="/matches">Matches</Link></li>
-            <li><Link to="/tournaments">Tournaments</Link></li>
-          </ul>
-        </nav>
-        <div className="search-account">
-          <input type="text" placeholder="Search..." className="search-bar" />
-          <button className="login-button">Login</button>
-        </div>
-      </header>
+  <header className="App-header">
+    <div className="logo-container">
+      <img src={logo} alt="CricFreak Logo" className="App-logo" />
+      <h1>CRICFREAK</h1>
+    </div>
+    <nav className="App-navbar">
+      <ul>
+        <li><Link to="/teams">Teams</Link></li>
+        <li><Link to="/players">Players</Link></li>
+        <li><Link to="/matches">Matches</Link></li>
+        <li><Link to="/tournaments">Tournaments</Link></li>
+      </ul>
+    </nav>
+    <div className="search-account">
+      <input type="text" placeholder="Search..." className="search-bar" />
+      <a href="/login">
+        <button className="login-button">Login</button>
+      </a>
+    </div>
+  </header>
 );
 
 export default Navbar;
